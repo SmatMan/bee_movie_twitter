5f1ee2bb7caba34141cc128d91291b99a705173a
bren <gamingajun10@gmail.com>
2020-10-22 19:44:53 -0400

    update script for every word

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import tweepy
 import config as cfg
 import time
+import datetime
 
 auth = tweepy.OAuthHandler(cfg.ck, cfg.cs)
 auth.set_access_token(cfg.at, cfg.ats)
@@ -8,19 +9,17 @@ auth.set_access_token(cfg.at, cfg.ats)
 
 api = tweepy.API(auth)
 
-with open('script.txt') as f:
+with open('wordlist.txt') as f:
     for line in f:
         try:
-            api.update_status(line)
-            print(f"Sent {line} successfully")
+            api.update_status(f"{line.strip()} is cringe")
+            print(f'"{line}" sent successfully at {datetime.datetime.now()}.')
         except Exception as e:
             print("Could not send line '" + line + "'")
             print(str(e))
         time.sleep(60)
 
-print("")
-print("")
-print("")
 print("NGL WE KINDA DONE 🎉😳")
+print(f"Finished at {datetime.datetime.now()}.")
 
 
